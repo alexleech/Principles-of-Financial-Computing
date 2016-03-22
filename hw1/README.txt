a0a38c9dba214b46945c52fbde3157f2e7d03a06
Allen <allen03262001@gmail.com>
2016-03-23 00:59:25 +0800

    adding input format and readme file

---
+++ hw1/README.txt
@@ -0,0 +1,7 @@
+Just type:"python hw1.py" to run the code
+Sample input format: (separated by "," in every line)
+	0.053, 0.051, 0.049, 0.047
+	3, 2, 3, 102
+	0.3
+
+Thanks!
--- hw1/hw1.py
+++ hw1/hw1.py
@@ -1,12 +1,13 @@
-s = [0.053, 0.051, 0.049, 0.047]
-C = [3, 2, 3, 102]
-w = 0.3
+import sys
+#s = [0.053, 0.051, 0.049, 0.047]
+#C = [3, 2, 3, 102]
+#w = 0.3
 
 def getPV(s, C, n, w):
 	pv = 0
 	for i in range(n):
 		pv = pv + C[i]/( (1+s[i])**(i+w) )
-	print 'pv = '+str(pv)
+	#print 'pv = '+str(pv)
 	return pv
 def getMD(s, C, n, w):
 	duration = 0
@@ -15,12 +16,12 @@ def getMD(s, C, n, w):
 	#print 'duration = '+str(duration)
 	pv = getPV(s, C, n, w)
 	duration = duration / pv
-	print 'Macaulay duration = '+str(duration)
+	#print 'Macaulay duration = '+str(duration)
 	y = getYTM(s, C, n, w, pv)
 	duration = duration/(1+y)
 	print 'Modified duration = '+str(duration)
 	getConvexity(s, C, n, w, y, pv)
-	getPseudoConv(s, C, n, w, y, pv)
+	#getPseudoConv(s, C, n, w, y, pv)
 	return duration
 def getYTM(s, C, n, w, pv):
 	accuracy = 1000000
@@ -35,7 +36,7 @@ def getYTM(s, C, n, w, pv):
 		if err<minErr:
 			minErr = err
 			bestY = Yield-1
-	print 'yield = '+str(bestY)
+	#print 'yield = '+str(bestY)
 	return bestY
 def getConvexity(s, C, n, w, y, pv):
 	# should be 12.1813
@@ -44,7 +45,7 @@ def getConvexity(s, C, n, w, y, pv):
 		t = w+i
 		conv = conv + C[i]*(t+1)*t / ((1+s[i])**t)
 	conv = conv/pv/(1+y)**2
-	print 'conv = '+str(conv)
+	print 'Convexity = '+str(conv)
 def getPseudoConv(s, C, n, w, y, pv):
 	deltaY = 0.005
 	pv_plus = 0
@@ -55,11 +56,17 @@ def getPseudoConv(s, C, n, w, y, pv):
 	for i in range(n):
 		pv_minus = pv_minus + C[i]/(y-deltaY)**(w+i)
 	conv = (pv_plus+pv_minus-2*pv)/pv/deltaY/deltaY
-	print 'pseudo conv = '+str(conv)
+	#print 'pseudo conv = '+str(conv)
 
 if __name__ == '__main__':
-
+	s_input = raw_input("please type in the spot rates(s): ")
+	s = map(float, s_input.strip().split(","))
+	C_input = raw_input("please type in the cash flow(C): ")
+	C = map(float, C_input.split(","))
+	w_input = raw_input("please type in the w: ")
+	w = float(w_input)
 	n = len(s)
 	if len(C)!=n:
 		print "size of s and C inconsistent!"
+		sys.exit(1)
 	getMD(s, C, n, w)
