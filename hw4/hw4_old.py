9fcc7a30709ad449c03e9bf4d92bc7a90fbcc90b
Allen <allen03262001@gmail.com>
2016-06-15 21:01:42 +0800

    correct answer as the spec

--- hw4/hw4_old.py
+++ hw4/hw4_old.py
@@ -35,17 +35,13 @@ def BinomialCIR(data):
 	x_r = 2*sqrt(r)/s
 	deltaX = sqrt(deltaT)
 	optionDuration = n*t/T
-	gamma = sqrt(b*b+2*s*s)
-		
-	#print optionDuration
+	print optionDuration
 	# Initial Price
 	Price = np.ones(n+1)
 	Option = np.zeros((n+1, n+1))
 	# Binomial CIR model
 	transformToOption = False
-	tt = T
 	for j in reversed(range(n)):
-		tt = tt - deltaT
 		for i in range(j+1):
 			p = 1
 			x_prime = x_r + (j-2*i)*deltaX
@@ -64,21 +60,22 @@ def BinomialCIR(data):
 				p=1
 			discountFactor = 1 / exp(r_prime * deltaT)
 			#print "dis = "+str(discountFactor)
-			#Price[i] = (p*Price[i]+(1.0-p)*Price[i+1])*discountFactor
+			Price[i] = (p*Price[i]+(1.0-p)*Price[i+1])*discountFactor
+			'''if j<=optionDuration and not transformToOption:
+				Price[i] = max(X-Price[i], 0)
+				if i==j:
+					transformToOption = True
+	return 100*(X/exp(r*deltaT*optionDuration)-Price[0])
+			'''
 			if j<=optionDuration:
-				tmp = exp(gamma*(T-tt))-1
-				A = pow((2*gamma*exp((b+gamma)*(T-tt)/2) / ((b+gamma)*tmp+2*gamma)), 2*b*m/s/s)
-				B = 2*tmp / ((b+gamma)*tmp+2*gamma)		
-				Price[i] = A*exp(-B*r_prime)
 				cont = (p*Option[j+1][i]+(1.0-p)*Option[j+1][i+1])*discountFactor
-				#if cont>(X-Price[i]):
-				#	print "j = "+str(j)+" i = "+str(i)+" cont = "+str(cont)+" now = "+str(X-Price[i])
+				if cont>(X-Price[i]):
+					print "j = "+str(j)+" i = "+str(i)+" cont = "+str(cont)+" now = "+str(X-Price[i])
 				Option[j][i] = max(X-Price[i], cont, 0)
-				#print "j = "+str(j)+" i = "+str(i)+" option = "+str(Option[j][i])
 			#print "j = "+str(j)+" i = "+str(i)+" r = "+str(r_prime)+" p = "+str(p)+" $ = " +str(Price[i])
 			#print Option[j][i]
-	return 100*(Option[0][0])
-	#return 100*(X-Price[0])
+	return 100*(X-Price[0])
+	#return 100*(Option[0][0])
 
 if __name__ == '__main__':
 	if len(sys.argv) > 1:
